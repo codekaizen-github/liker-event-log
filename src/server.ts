cffe648c31be83ffd866b31923a4def6b2d52383
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-08-20 01:15:00 +0000

    Set isolation levels

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/fencingToken', async (req, res) => {
-    await db.transaction().execute(async (trx) => {
+    await db.transaction().setIsolationLevel('serializable').execute(async (trx) => {
         const result = await createStreamOut(trx, {
             data: JSON.stringify({ type: 'fencing-token-requested' }),
         });
@@ -42,7 +42,7 @@ app.get('/fencingToken', async (req, res) => {
 });
 
 app.post('/streamIn', async (req, res) => {
-    await db.transaction().execute(async (trx) => {
+    await db.transaction().setIsolationLevel('serializable').execute(async (trx) => {
         const insertData = {
             data: JSON.stringify(req.body.data),
         };
@@ -59,7 +59,7 @@ app.post('/streamIn', async (req, res) => {
 app.get('/streamOut', async (req, res) => {
     // Get the query parameter 'afterId' from the request
     const afterId = Number(req.query.afterId);
-    await db.transaction().execute(async (trx) => {
+    await db.transaction().setIsolationLevel('serializable').execute(async (trx) => {
         const records = await findStreamOutsGreaterThanStreamOutId(
             trx,
             afterId
@@ -71,7 +71,7 @@ app.get('/streamOut', async (req, res) => {
 });
 
 app.post('/httpSubscriber/register', async (req, res) => {
-    await db.transaction().execute(async (trx) => {
+    await db.transaction().setIsolationLevel('serializable').execute(async (trx) => {
         const existing = await findHttpSubscribers(trx, {
             url: req.body.url,
         });
@@ -84,7 +84,7 @@ app.post('/httpSubscriber/register', async (req, res) => {
 });
 
 app.post('/httpSubscriber/unregister', async (req, res) => {
-    await db.transaction().execute(async (trx) => {
+    await db.transaction().setIsolationLevel('serializable').execute(async (trx) => {
         const existing = await findHttpSubscribers(trx, {
             url: req.body.url,
         });
@@ -107,7 +107,7 @@ app.listen(port, () => {
 
 // Get the most recent log record and notify subscribers
 (async () => {
-    await db.transaction().execute(async (trx) => {
+    await db.transaction().setIsolationLevel('serializable').execute(async (trx) => {
         const record = await getMostRecentStreamOut(trx);
         if (record === undefined) {
             return;

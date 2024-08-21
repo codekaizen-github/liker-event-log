3cdad80625e3e1eb4815f8302ac6d62255cf549f
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-08-21 00:42:29 +0000

    Use jsonStrings

--- src/database.ts
+++ src/database.ts
@@ -4,16 +4,16 @@ import { Kysely, MysqlDialect } from "kysely";
 import { env } from "process";
 import e from "express";
 
-const poolConfig = {
-	database: env.LIKER_EVENT_LOG_DB_NAME,
-	host: env.LIKER_EVENT_LOG_DB_HOSTNAME,
-	user: env.LIKER_EVENT_LOG_DB_USER,
-	password: env.LIKER_EVENT_LOG_DB_PASSWORD,
-	port: 3306,
-	connectionLimit: 10,
-};
 const dialect = new MysqlDialect({
-	pool: createPool(poolConfig),
+	pool: createPool({
+		database: env.LIKER_EVENT_LOG_DB_NAME,
+		host: env.LIKER_EVENT_LOG_DB_HOSTNAME,
+		user: env.LIKER_EVENT_LOG_DB_USER,
+		password: env.LIKER_EVENT_LOG_DB_PASSWORD,
+		port: 3306,
+		connectionLimit: 10,
+		jsonStrings: true,
+	}),
 });
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely
